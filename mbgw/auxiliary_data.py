76d10bd7475731148577d2a08c78a057c9264c8f
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-03-10 17:48:11 +0000

    Moved fst_cov_fun.f into appropriate directory

--- mbgw/auxiliary_data.py
+++ mbgw/auxiliary_data.py
@@ -6,7 +6,6 @@ try:
     data_dict = {}
     for fname in os.listdir(__root__):
         if fname[-4:]=='hdf5':
-            # print fname
             data_dict[fname[:-5]] = tables.openFile('%s/%s'%(__root__,fname)).root
     locals().update(data_dict)
     age_dist_model = age_dist_model.chain1.PyMCsamples
@@ -14,9 +13,3 @@ try:
 except:
     cls, inst, tb = sys.exc_info()
     print 'Warning: unable to import auxiliary data. Error message: \n'+inst.message
-        # exec('%s=tables.openFile(%s/%s)'%(fname[:-5],__root__,fname))
-# urb = tables.openFile(__root__+'/urb.hdf5').root
-# periurb = tables.openFile(__root__+'/periurb.hdf5').root
-# parameter_model = tables.openFile(__root__+'/parameter_model.hdf5').root.chain1.PyMCsamples
-# age_dist_model = tables.openFile(__root__+'/age_dist_model.hdf5').root.chain1.PyMCsamples
-# ndvi = tables.openFile(__root__+'/mbg-ndvi.hdf5').root
--- st_cov_fun/setup.py
+++ st_cov_fun/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('st_cov_fun',parent_package,top_path)
-    config.add_extension(name='fst_cov_fun',sources=['fst_cov_fun.f'])
+    config.add_extension(name='fst_cov_fun',sources=['st_cov_fun/fst_cov_fun.f'])
     config.make_config_py()
     return config
 
