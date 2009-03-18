dae257da03db1f3a260194dcfae1dbd71d53b9f2
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-03-18 15:56:34 +0000

    Made auxiliary data more forgiving

--- mbgw/auxiliary_data.py
+++ mbgw/auxiliary_data.py
@@ -1,15 +1,16 @@
 import tables
 import os, sys
 import mbgw
-try:
-    __root__ = mbgw.__path__[0] + '/../datafiles/auxiliary_data'
-    data_dict = {}
-    for fname in os.listdir(__root__):
-        if fname[-4:]=='hdf5':
+
+__root__ = mbgw.__path__[0] + '/../datafiles/auxiliary_data'
+data_dict = {}
+for fname in os.listdir(__root__):
+    if fname[-4:]=='hdf5':
+        try:
             data_dict[fname[:-5]] = tables.openFile('%s/%s'%(__root__,fname)).root
-    locals().update(data_dict)
-    age_dist_model = age_dist_model.chain1.PyMCsamples
-    parameter_model = parameter_model.chain1.PyMCsamples
-except:
-    cls, inst, tb = sys.exc_info()
-    print 'Warning: unable to import auxiliary data. Error message: \n'+inst.message
+        except:
+            cls, inst, tb = sys.exc_info()
+            print 'Warning: unable to import auxiliary data. Error message: \n'+inst.message
+locals().update(data_dict)
+age_dist_model = age_dist_model.chain1.PyMCsamples
+parameter_model = parameter_model.chain1.PyMCsamples
