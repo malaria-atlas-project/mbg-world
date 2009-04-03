c7354a00ac78ce96f4bb649d500ec145e121f18b
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-04-03 12:22:37 +0100

    Incorporated working condor-zeus script'

--- mbgw-scripts/joint-sim-condor-zeus.py
+++ mbgw-scripts/joint-sim-condor-zeus.py
@@ -3,47 +3,51 @@ import pymc as pm
 import mbgw
 from mbgw.joint_simulation import *
 import tables as tb
-from mbgw.master_grid import *
+import mbgw.master_grid as mg
 import sys
 
 print 'Imports done'
 
 # Establish blocks based on spatial distance only.
-n_blocks_x = 20
-n_blocks_y = 20
+n_blocks_x = 10
+n_blocks_y = 10
+N_nearest = 300
 
-# i = int(sys.argv[1])
-# iter_per_job = int(sys.argv[2])
-# n_jobs = int(sys.argv[3])
+i = int(sys.argv[1])
+iter_per_job = int(sys.argv[2])
+n_jobs = int(sys.argv[3])
+region = sys.argv[4]
+fname = sys.argv[5]
+burn = int(sys.argv[6])
+grid_lims = getattr(mg, region + '_lims')
+nmonths = 288 
+start_year = 1985
+mask_name = 'landSea-e'
+relp=1e-3
 
-i = 0
-iter_per_job = 3
-n_jobs = 5
+hf = tb.openFile(fname)
+n_total = len(hf.root.chain0.PyMCsamples)
+indices = np.array(np.linspace(burn, n_total, n_jobs+1), dtype=int)
+my_start = indices[i]
+my_end = indices[i+1]
 
-def simulate(fname, grid_lims, nmonths, start_year, burn, mask_name, relp=1e-3):
-    """
-    n = number of realizations desired
-    fname = path to trace file
-    grid_lims = [[lon_llc lon_urc] [lat_llc lat_urc]]
-    nmonths = number of months to realize
-    start_month = starting year
-    burn = number of burnin iterations to discard
-    outfile_name = name of hdf5 archive
-    relp = covariance's relative precision, make it pretty big for speed.
-    """
-    hf = tb.openFile(fname)
-    n_total = len(hf.root.chain0.PyMCsamples)
-    indices = np.array(np.linspace(burn, n_total, n_jobs+1), dtype=int)
-    my_start = indices[i]
-    my_end = indices[i+1]
+infile_base = fname.split('/')[-1].replace('.hdf5','')
+outfile_name = '/share/scratch/malaria-atlas-project/MAP-outputs/realizations_nb_%i_%s.hdf5'%(n_blocks_x,'_'.join([infile_base, 'iterations', str(i*iter_per_job), str((i+1)*iter_per_job)]))
+# print ('create_many_realizations(%i, %i, hf.root.chain0, hf.root.metadata, grid_lims, start_year, nmonths, n_blocks_x, n_blocks_y, %s, relp, mask_fname, n_in_trace=%i)'%(my_start, iter_per_job, outfile_name, my_end))
 
-    infile_base = fname.split('/')[-1].replace('.hdf5','')
-    outfile_name = '/share/scratch/zoo-images/MAP-outputs/realizations_%s.hdf5'%('_'.join([infile_base]+[str(j) for j in indices]))
-    print ('create_many_realizations(%i, %i, hf.root.chain0, hf.root.metadata, grid_lims, start_year, nmonths, n_blocks_x, n_blocks_y, %s, relp, mask_fname, n_in_trace=%i)'%(my_start, iter_per_job, outfile_name, my_end))
+print 'i: %i'%i
+print 'iter_per_job: %i'%i
+print 'n_jobs: %i'%i
+print 'region: %s'%s
+print 'fname: %s'%s
+print 'burn: %i'%i
+print 'nmonths: %i'%n_months
+print 'start_year: %i'%start_year
+print 'mask_name: %s'%mask_name
+print 'relp: %f'%relp
+print 'grid_lims: %s'%str(grid_lims)
+print 'n_blocks_x: %i'%n_blocks_x
+print 'n_blocks_y: %i'%n_blocks_y
+print 'N_nearest: %i'%N_nearest
 
-    # create_many_realizations(my_start, iter_per_job, hf.root.chain0, hf.root.metadata, grid_lims, start_year, nmonths, n_blocks_x, n_blocks_y, outfile_name, relp, mask_name, n_in_trace = my_end)
-
-
-
-# Generate simulation
-simulate( '../datafiles/good-traces/QRYPFPR010708_Africa_Run_9.10.2008.hdf5', AF_lims, 288, 1985, 2000, 'landSea-e')
+create_many_realizations(my_start, iter_per_job, hf.root.chain0, hf.root.metadata, grid_lims, start_year, nmonths, n_blocks_x, n_blocks_y, outfile_name, N_nearest, relp, mask_name, n_in_trace = my_end)
