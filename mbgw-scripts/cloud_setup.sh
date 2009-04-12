401b771e49d585283035221a82468eceee02fcbf
PWG <peter.gething@zoo.ox.ac.uk>
2009-04-12 14:25:15 +0100

    ongoing tweaks towards EC2 functionality - uploaded new files

---
+++ mbgw-scripts/cloud_setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+apt-get install screen
+
+cd /usr/lib/python2.5/site-packages/
+rm -r -f pymc*
+cd
+svn checkout http://pymc.googlecode.com/svn/trunk/ pymc
+cd pymc
+python setupegg.py install
+cd 
+
+apt-get install python-boto
+
+rm -r -f mbg-world
+git clone git://github.com/malaria-atlas-project/mbg-world.git
+cd mbg-world
+ln -s ../datafiles datafiles
+python setup.py develop
+cd
+
+rm -r -f st-cov-fun
+git clone git://github.com/malaria-atlas-project/st-cov-fun.git
+cd st-cov-fun
+f2py -c fst_cov_fun.f -m fst_cov_fun
+python setup.py install
+cd
+
+rm -r -f map_utils
+git clone git://github.com/malaria-atlas-project/map_utils.git
+cd map_utils
+python setup.py install
+cd
