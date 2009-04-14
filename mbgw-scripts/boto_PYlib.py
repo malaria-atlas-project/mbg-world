d313b585c849f2643351beda5456dbea542e434c
PWG <peter.gething@zoo.ox.ac.uk>
2009-04-14 17:40:12 +0100

    minor

--- mbgw-scripts/boto_PYlib.py
+++ mbgw-scripts/boto_PYlib.py
@@ -23,7 +23,7 @@ from map_utils import checkAndBuildPaths
 ##############################################################################################################################
 def S3connectKeys(keyNo):
 
-    os.listdir('/root/')
+    print(os.listdir('/root/'))
     a=file(keyPath).read()
     key = a.split(',')[keyNo]
     key=key.strip("\'")
