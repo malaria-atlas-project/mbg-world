6ad09d9a390dbe8d5912660a2fc6963a8691fdc0
PWG <peter.gething@zoo.ox.ac.uk>
2009-04-14 18:28:08 +0100

    minor

--- mbgw-scripts/extract_params.py
+++ mbgw-scripts/extract_params.py
@@ -7,7 +7,7 @@ utilFolder = '/map_utils/'
 keyPath = '/root/s3code.txt' 
 
 # main input hdf5  file of simulated realisations of f
-realisations_path = '/root/realizations/realizations_QRYPFPR010708_Africa_Run_9.10.2008_iterations_FILESTARTREL_FILEENDREL.hdf5'
+realisations_path = 'root/realizations/realizations_QRYPFPR010708_Africa_Run_9.10.2008_iterations_FILESTARTREL_FILEENDREL.hdf5'
 #filename = '/home/pwg/Desktop/test_sim_KE.hdf5'
 
 # location for export of raw extractions (as they come off each distributed instance)
