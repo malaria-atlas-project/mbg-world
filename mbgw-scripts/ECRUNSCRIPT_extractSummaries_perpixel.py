8f7feb7be618ba0711db6b71e65cdd0f38a55354
PWG <peter.gething@zoo.ox.ac.uk>
2009-05-12 16:49:59 +0100

    minor

--- mbgw-scripts/ECRUNSCRIPT_extractSummaries_perpixel.py
+++ mbgw-scripts/ECRUNSCRIPT_extractSummaries_perpixel.py
@@ -52,42 +52,42 @@ print "startRel: "+str(startRel)
 print "endRel: "+str(endRel)
 print "BURDEN: "+str(BURDEN)
 
-# download this realisation file from S3 storage
-print '\nDownloading realisation from S3..'
-S3bucketname = hdf5block_path.split('/')[-2]
-print '\tS3bucketname: '+str(S3bucketname)
-S3filename = hdf5block_path.split('/')[-1]
-print '\tS3filename: '+str(S3filename)
-S.downloadFileFromBucket(S3bucketname,S3filename,hdf5block_path,overwriteContent=False,makeDirectory=True,VERBOSE=True)
-checkAndBuildPaths(hdf5block_path,VERBOSE=False,BUILD=False)
-
-# download from S3 the other necessary file (optionally need 5km grump for burden map)
-if (BURDEN==True):
-    print '\nDownloading other files from S3..'
-    S3bucketname = gr005km_path.split('/')[-2]
-    print '\tS3bucketname: '+str(S3bucketname)
-    S3filename = gr005km_path.split('/')[-1]
-    print '\tS3filename: '+str(S3filename)
-    S.downloadFileFromBucket(S3bucketname,S3filename,gr005km_path,overwriteContent=False,makeDirectory=True,VERBOSE=True)
-    checkAndBuildPaths(gr005km_path,VERBOSE=False,BUILD=False)
-
-# check path for exports exists
-print '\nchecking path for export exists..'
-checkAndBuildPaths(exportPathDistributed_perpixel,VERBOSE=True,BUILD=True)
-
-# now call extractSummaries_perpixel substituting in the formatted sys args 
-print '\nrunning extractSummaries_perpixel..'
-extractSummaries_perpixel ([slice(None,None,None), slice(None,None,None), slice(0,12,None)],2,10,n_per,FileStartRel,FileEndRel,totalN,startRel,endRel,BURDEN)
+## download this realisation file from S3 storage
+#print '\nDownloading realisation from S3..'
+#S3bucketname = hdf5block_path.split('/')[-2]
+#print '\tS3bucketname: '+str(S3bucketname)
+#S3filename = hdf5block_path.split('/')[-1]
+#print '\tS3filename: '+str(S3filename)
+#S.downloadFileFromBucket(S3bucketname,S3filename,hdf5block_path,overwriteContent=False,makeDirectory=True,VERBOSE=True)
+#checkAndBuildPaths(hdf5block_path,VERBOSE=False,BUILD=False)
+#
+## download from S3 the other necessary file (optionally need 5km grump for burden map)
+#if (BURDEN==True):
+#    print '\nDownloading other files from S3..'
+#    S3bucketname = gr005km_path.split('/')[-2]
+#    print '\tS3bucketname: '+str(S3bucketname)
+#    S3filename = gr005km_path.split('/')[-1]
+#    print '\tS3filename: '+str(S3filename)
+#    S.downloadFileFromBucket(S3bucketname,S3filename,gr005km_path,overwriteContent=False,makeDirectory=True,VERBOSE=True)
+#    checkAndBuildPaths(gr005km_path,VERBOSE=False,BUILD=False)
+#
+## check path for exports exists
+#print '\nchecking path for export exists..'
+#checkAndBuildPaths(exportPathDistributed_perpixel,VERBOSE=True,BUILD=True)
+#
+## now call extractSummaries_perpixel substituting in the formatted sys args 
+#print '\nrunning extractSummaries_perpixel..'
+#extractSummaries_perpixel ([slice(None,None,None), slice(None,None,None), slice(0,12,None)],2,10,n_per,FileStartRel,FileEndRel,totalN,startRel,endRel,BURDEN)
 
 # now upload the output back to the S3 storage
 print '\nrunning extractSummaries_perpixel..'
 #S.uploadDirectoryAsBucket('distributedoutput_perpixel',exportPathDistributed_perpixel,uploadConstituentFiles=True,overwriteContent=True)
 
 ## loop through all files in local export storage
-for fname in os.listdir(exportPathDistributed_country):
+for fname in os.listdir(exportPathDistributed_perpixel):
 
     # if file contains string indicating an outcome from this realisation set, then upload to S3
     if (fname.find('r'+str(FileStartRel)+'to'+str(FileEndRel)+'.gz')>0):
-        filepath = exportPathDistributed_country+fname
+        filepath = exportPathDistributed_perpixel+fname
         print 'uploading '+filepath+' to S3 bucket '+'distributedoutput_perpixel'
         S.uploadFileToBucket('distributedoutput_perpixel',filePath,overwriteContent=True,makeBucket=True,VERBOSE=True)
