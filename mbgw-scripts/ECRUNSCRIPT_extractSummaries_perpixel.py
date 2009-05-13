32660c187bc33fca8f0bd314ea9e05fb623d0148
PWG <peter.gething@zoo.ox.ac.uk>
2009-05-13 19:24:56 +0100

    minor

--- mbgw-scripts/ECRUNSCRIPT_extractSummaries_perpixel.py
+++ mbgw-scripts/ECRUNSCRIPT_extractSummaries_perpixel.py
@@ -39,7 +39,7 @@ print "FileEndRel: "+str(FileEndRel)
 print "totalN: "+str(totalN)
 print "startRel: "+str(startRel)
 print "endRel: "+str(endRel)
-print "BURDEN: "+str(BURDEN)
+print "BURDEN: "+str(BURDEN) 
 
 # download this realisation file from S3 storage
 print '\nDownloading realisation from S3..'
@@ -54,30 +54,30 @@ checkAndBuildPaths(hdf5block_path,VERBOSE=False,BUILD=False)
 print '\nchecking path for export exists..'
 checkAndBuildPaths(exportPathDistributed_perpixel,VERBOSE=True,BUILD=True)
 
-# now call extractSummaries_perpixel substituting in the formatted sys args 
-print '\nrunning extractSummaries_perpixel..'
-extractSummaries_perpixel ([slice(None,None,None), slice(None,None,None), slice(0,12,None)],2,10,n_per,FileStartRel,FileEndRel,totalN,startRel,endRel,BURDEN)
-
-# now upload the output back to the S3 storage
-print '\nrunning extractSummaries_perpixel..'
-#S.uploadDirectoryAsBucket('distributedoutput_perpixel',exportPathDistributed_perpixel,uploadConstituentFiles=True,overwriteContent=True)
-
-## loop through all files in local export storage
-for fname in os.listdir(exportPathDistributed_perpixel):
-
-    # if file contains string indicating an outcome from this realisation set, then upload to S3 (try a few times before failing this section)
-    if (fname.find('r'+str(FileStartRel)+'to'+str(FileEndRel)+'.gz')>0):
-        filepath = exportPathDistributed_perpixel+fname
-        print 'uploading '+filepath+' to S3 bucket distributedoutput_perpixel'
-        failCount = 0
-        while failCount<=3:
-            try:
-                S.uploadFileToBucket('distributedoutput_perpixel',filepath,overwriteContent=True,makeBucket=True,VERBOSE=True)
-                break
-            except RuntimeError:
-                failCount+=1 
-                if failCount<=3:
-                    print 'upload of '+filepath+' to S3 bucket distributedoutput_perpixel failed '+str(failCount) +' times: retrying..'
-                else:
-                    print 'upload of '+filepath+' to S3 bucket distributedoutput_perpixel failed '+str(failCount) +' times: GIVING UP - FILE WILL NOT BE COPIED!!'
+## now call extractSummaries_perpixel substituting in the formatted sys args 
+#print '\nrunning extractSummaries_perpixel..'
+#extractSummaries_perpixel ([slice(None,None,None), slice(None,None,None), slice(0,12,None)],2,10,n_per,FileStartRel,FileEndRel,totalN,startRel,endRel,BURDEN)
+#
+## now upload the output back to the S3 storage
+#print '\nrunning extractSummaries_perpixel..'
+##S.uploadDirectoryAsBucket('distributedoutput_perpixel',exportPathDistributed_perpixel,uploadConstituentFiles=True,overwriteContent=True)
+#
+### loop through all files in local export storage
+#for fname in os.listdir(exportPathDistributed_perpixel):
+#
+#    # if file contains string indicating an outcome from this realisation set, then upload to S3 (try a few times before failing this section)
+#    if (fname.find('r'+str(FileStartRel)+'to'+str(FileEndRel)+'.gz')>0):
+#        filepath = exportPathDistributed_perpixel+fname
+#        print 'uploading '+filepath+' to S3 bucket distributedoutput_perpixel'
+#        failCount = 0
+#        while failCount<=3:
+#            try:
+#                S.uploadFileToBucket('distributedoutput_perpixel',filepath,overwriteContent=True,makeBucket=True,VERBOSE=True)
+#                break
+#            except RuntimeError:
+#                failCount+=1 
+#                if failCount<=3:
+#                    print 'upload of '+filepath+' to S3 bucket distributedoutput_perpixel failed '+str(failCount) +' times: retrying..'
+#                else:
+#                    print 'upload of '+filepath+' to S3 bucket distributedoutput_perpixel failed '+str(failCount) +' times: GIVING UP - FILE WILL NOT BE COPIED!!'
                 
