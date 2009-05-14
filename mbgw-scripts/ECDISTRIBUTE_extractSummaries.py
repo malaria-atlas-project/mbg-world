dfe1b82e009664a9281cfb9706f3b3a6258e91d5
PWG <peter.gething@zoo.ox.ac.uk>
2009-05-14 16:31:21 +0100

    now include both _perpixel and _country extractions in same ECDISTRIBUTE and ECRUNSCRIPTs

---
+++ mbgw-scripts/ECDISTRIBUTE_extractSummaries.py
@@ -0,0 +1,53 @@
+# import libraries
+from amazon_ec import *
+from boto_PYlib import *
+import numpy as np
+
+# initialise amazon S3 key object 
+S=S3(keyPath='/home/pwg/mbg-world/mbgw-scripts/s3code.txt')
+
+# define ID of reservation that contains the instances we will use on EC2
+RESERVATIONID = 'r-291c8e40'
+
+# set job distribution parameters
+NINSTANCES = 1
+MAXJOBSPERINSTANCE = 1
+MAXJOBTRIES = 1 #maximum number of tries before we give up on any individual job
+STDOUTPATH = '/home/pwg/mbg-world/extraction/DistributedOutputSTDOUTERR/'
+
+# set path to realisations on S3 and extract bucket and generic file name
+realisations_path = '/mnt/qrypfpr010708_africa_run_9.10.2008_trial_two/realizations_mem_100000000_QRYPFPR010708_Africa_Run_9.10.2008_iterations_FILESTARTREL_FILEENDREL.hdf5'
+relBucket = realisations_path.rsplit('/')[-2]
+relPath = realisations_path.rsplit('/')[-1]
+
+# call queryRealizationsInBucket to obtain number and start/end realisation numbers of these realisation files
+relDict = S.queryRealizationsInBucket(relBucket,relPath,VERBOSE=True)
+
+# set realization number parameters
+NRELS = relDict['Nrealisations']
+NJOBS = relDict['Nfiles']
+
+####################################TEMP
+NJOBS = 1
+####################################TEMP
+
+FileStartRels = relDict['StartRelList']
+FileEndRels = relDict['EndRelList']
+NPER  = 2
+NTOTALREL = NRELS*NPER
+
+####################################TEMP 
+NTOTALREL = 2
+####################################TEMP
+
+# define files to upload to instance (from local machine) before any execution
+UPLOADFILES=['amazon_joint_sim.py','/home/pwg/mbg-world/mbgw-scripts/cloud_setup.sh','/home/pwg/mbg-world/mbgw-scripts/s3code.txt']
+
+# define any initialisation commands to exctue on instance before main job
+INITCMDS=['bash /root/cloud_setup.sh','"cd mbg-world/mbgw-scripts/;python ECRUNSCRIPT_extractSummaries_PREDOWNLOAD.py True True True"']
+
+# construct main commands list
+CMDS = ['"cd mbg-world/mbgw-scripts/;python ECRUNSCRIPT_extractSummaries.py %i %i %i %i None None True True True"'%(NPER,int(FileStartRels[i]),int(FileEndRels[i]),NTOTALREL) for i in xrange(NJOBS)]
+
+# finally, call local function map_jobs from amazon_ec module to distribute these jobs on EC2
+returns = map_jobs(RESERVATIONID,NINSTANCES,MAXJOBSPERINSTANCE,MAXJOBTRIES,cmds=CMDS, init_cmds=INITCMDS,upload_files=UPLOADFILES, interval=20,shutdown=False,STDOUTPATH=STDOUTPATH)    
---
+++ mbgw-scripts/ECRUNSCRIPT_extractSummaries_PREDOWNLOAD.py
@@ -0,0 +1,54 @@
+# script to download to an instance, before anything execute,  any necessary auxilliary files, and to 
+# pre-build any necessary directories
+
+print 'Starting: ECRUNSCRIPT_extractSummaries_perpixel_PREDOWNLOAD..'
+
+# import libraries
+from map_utils import checkAndBuildPaths
+from boto_PYlib import *
+from extract_params import *
+
+S=S3() # initialise key object
+
+# deal with system arguments
+BURDEN = bool(sys.argv[1])
+PERPIXEL = bool(sys.argv[2])
+PERCOUNTRY = bool(sys.argv[3])
+
+# make empty directory on instance to house realisation hdf5 file downloaded from S3
+print '\n\tBuilding directory: '+realisations_path.rpartition('/')[0]
+checkAndBuildPaths(realisations_path.rpartition('/')[0],VERBOSE=True,BUILD=True)
+
+if (PERPIXEL==True):
+    # make empty directory on instance to house output files ready to be uploaded back to S3
+    print '\n\tBuilding directory: '+exportPathDistributed_perpixel
+    checkAndBuildPaths(exportPathDistributed_perpixel,VERBOSE=True,BUILD=True)    
+
+    if (BURDEN==True):
+        print '\n\tDownloading auxilliary file to '+str(grump5km_path)+' from S3..'
+        S3bucketname = grump5km_path.split('/')[-2]
+        print '\t\tS3bucketname: '+str(S3bucketname)
+        S3filename = grump5km_path.split('/')[-1]
+        print '\t\tS3filename: '+str(S3filename)
+        S.downloadFileFromBucket(S3bucketname,S3filename,grump5km_path,overwriteContent=False,makeDirectory=True,VERBOSE=True)
+
+if (PERCOUNTRY==True):
+    # make empty directory on instance to house output files ready to be uploaded back to S3
+    print '\n\tBuilding directory: '+exportPathDistributed_country
+    checkAndBuildPaths(exportPathDistributed_country,VERBOSE=True,BUILD=True)  
+
+    print '\n\tDownloading auxilliary file to '+str(grump1km_path)+' from S3..'
+    S3bucketname = grump1km_path.split('/')[-2]
+    print '\t\tS3bucketname: '+str(S3bucketname)
+    S3filename = grump1km_path.split('/')[-1]
+    print '\t\tS3filename: '+str(S3filename)
+    S.downloadFileFromBucket(S3bucketname,S3filename,grump1km_path,overwriteContent=False,makeDirectory=True,VERBOSE=True)
+
+    print '\n\tDownloading auxilliary file to '+str(salblim1km_path)+' from S3..'    
+    S3bucketname = salblim1km_path.split('/')[-2]    
+    print '\t\tS3bucketname: '+str(S3bucketname)    
+    S3filename = salblim1km_path.split('/')[-1]
+    print '\t\tS3filename: '+str(S3filename)
+    S.downloadFileFromBucket(S3bucketname,S3filename,salblim1km_path,overwriteContent=False,makeDirectory=True,VERBOSE=True)
+
+print '\nDONE: ECRUNSCRIPT_extractSummaries_PREDOWNLOAD'
