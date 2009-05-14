53441cb2079d8f2abd3d974a4540fa249405cd38
PWG <peter.gething@zoo.ox.ac.uk>
2009-05-14 18:07:22 +0100

    combineDistributedExtractions now does both -perpixel and -coutry

--- mbgw-scripts/ECDISTRIBUTE_combineDistribExtractions.py
+++ mbgw-scripts/ECDISTRIBUTE_combineDistribExtractions.py
@@ -7,22 +7,22 @@ import numpy as np
 #S=S3(keyPath='/home/pwg/mbg-world/mbgw-scripts/s3code.txt')
 
 # define ID of reservation that contains the instances we will use on EC2
-RESERVATIONID = 'r-a55fcdcc'
+RESERVATIONID = 'r-6d30a204'
 
 # set job distribution parameters
 NINSTANCES = 1
 MAXJOBSPERINSTANCE = 1
 MAXJOBTRIES = 3 #maximum number of tries before we give up on any individual job
-STDOUTPATH = '/home/pwg/mbg-world/extraction/CombinedOutput_perpixel/'
-
-# construct commands list
-CMDS = ['"cd mbg-world/mbgw-scripts/;python ECRUNSCRIPT_combineDistribExtractions_perpixel.py True"']
+STDOUTPATH = '/home/pwg/mbg-world/extraction/CombinedOutputSTDERROUT/'
 
 # define files to upload to instance before any execution
 UPLOADFILES=['amazon_joint_sim.py','/home/pwg/mbg-world/mbgw-scripts/cloud_setup.sh','/home/pwg/mbg-world/mbgw-scripts/s3code.txt']
 
 # define any initialisation commands to exctue on instance before main job
 INITCMDS=['bash /root/cloud_setup.sh']
+
+# construct commands list
+CMDS = ['"cd mbg-world/mbgw-scripts/;python ECRUNSCRIPT_combineDistribExtractions.py True True True"']
 
 # finally, call local function map_jobs from amazon_ec module to distribute these jobs on EC2
 returns = map_jobs(RESERVATIONID,NINSTANCES,MAXJOBSPERINSTANCE,MAXJOBTRIES,cmds=CMDS, init_cmds=INITCMDS,upload_files=UPLOADFILES, interval=20,shutdown=False,STDOUTPATH=STDOUTPATH)    
---
+++ mbgw-scripts/ECRUNSCRIPT_combineDistribExtractions.py
@@ -0,0 +1,93 @@
+print "STARTING: ECRUNSCRIPT_combineDistribExtractions..\n"
+
+# import libraries
+from map_utils import checkAndBuildPaths
+from extract_combineExtractions import *
+from boto_PYlib import *
+from extract_params import *
+
+S=S3() # initialise key object
+
+# deal with system arguments
+BURDEN = bool(sys.argv[1])
+PERPIXEL = bool(sys.argv[2])
+PERCOUNTRY = bool(sys.argv[3])
+
+if PERPIXEL==True:
+
+    # download from S3 contents of bucket 'distributedoutput_perpixel', will automatically build the local directory if necessary
+    print 'n\t\Downloading contents of S3 bucket "distributedoutput_perpixel" to local directory '+exportPathDistributed_perpixel
+    S.downloadBucketContents('distributedoutput_perpixel',exportPathDistributed_perpixel,overwriteContent=False,VERBOSE=True)
+
+    # build path for output to house combined per-pixel output maps
+    print 'n\t\Checking path for '+exportPathCombined_perpixel
+    checkAndBuildPaths(exportPathCombined_perpixel,VERBOSE=True,BUILD=True)
+
+    # download from S3 the other necessary files (optionally need 5km grump for burden map)
+    print '\n\tDownloading lim5kmbnry file from S3..'
+    S3bucketname = lim5kmbnry_path.split('/')[-2]
+    print '\t\tS3bucketname: '+str(S3bucketname)
+    S3filename = lim5kmbnry_path.split('/')[-1]
+    print '\t\tS3filename: '+str(S3filename)
+    S.downloadFileFromBucket(S3bucketname,S3filename,lim5kmbnry_path,overwriteContent=False,makeDirectory=True,VERBOSE=True)
+    checkAndBuildPaths(lim5kmbnry_path,VERBOSE=True,BUILD=False)
+
+    if (BURDEN==True):
+        print '\n\tDownloading grump5km file from S3..'
+        S3bucketname = grump5km_path.split('/')[-2]
+        print '\t\tS3bucketname: '+str(S3bucketname)
+        S3filename = grump5km_path.split('/')[-1]
+        print '\t\tS3filename: '+str(S3filename)
+        S.downloadFileFromBucket(S3bucketname,S3filename,grump5km_path,overwriteContent=False,makeDirectory=True,VERBOSE=True)
+        checkAndBuildPaths(grump5km_path,VERBOSE=True,BUILD=False)
+
+    # now call extractSummaries_perpixel substituting in the formatted sys args 
+    print '\n\tCalling combineDistribExtractions_perpixel'
+    combineDistribExtractions_perpixel()
+
+    # now upload the output back to the S3 storage
+
+    print '\n\tuploading contents of exportPathCombined_perpixel to S3 bucket combinedoutput_perpixel'
+    failCount = 0
+    while failCount<=3:
+        try:
+            S.uploadDirectoryAsBucket('combinedoutput_perpixel',exportPathCombined_perpixel,uploadConstituentFiles=True,overwriteContent=True)
+            break
+        except RuntimeError:
+            failCount+=1 
+            if failCount<=3:
+                print '\t\tuploading contents of exportPathCombined_perpixel to S3 bucket CombinedOutput_perpixel failed '+str(failCount) +' times: retrying..'
+            else:
+                print '\t\tuploading contents of exportPathCombined_perpixel to S3 bucket CombinedOutput_perpixel failed '+str(failCount) +' times: GIVING UP - FILE CONTENTS MAY NOT ALL HAVE COPIED!!'
+
+if PERCOUNTRY==True:
+
+    # download from S3 contents of bucket 'distributedoutput_country', will automatically build the local directory if necessary
+    print 'n\t\Downloading contents of S3 bucket "distributedoutput_country" to local directory '+exportPathDistributed_perpixel
+    S.downloadBucketContents('distributedoutput_country',exportPathDistributed_country,overwriteContent=False,VERBOSE=True)
+
+    # build path for output to house combined per-pixel output maps
+    print 'n\t\Checking path for '+exportPathCombined_country
+    checkAndBuildPaths(exportPathCombined_country,VERBOSE=True,BUILD=True)
+
+    # now call extractSummaries_country substituting in the formatted sys args 
+    print '\n\tCalling combineDistribExtractions_country'
+    combineDistribExtractions_country()
+            
+    # now upload the output back to the S3 storage            
+
+    print '\n\tuploading contents of '+exportPathCombined_country+'to S3 bucket '+str(exportPathCombined_country.split('/')[-2].lower())
+    failCount = 0
+    while failCount<=3:
+        try:
+            S.uploadDirectoryAsBucket(exportPathCombined_country.split('/')[-2].lower(),exportPathCombined_country,uploadConstituentFiles=True,overwriteContent=True)
+            break
+        except RuntimeError:
+            failCount+=1 
+            if failCount<=3:
+                print '\t\tuploading contents of '+exportPathCombined_country+' to S3 bucket '+exportPathCombined_country.split('/')[-2].lower()+' failed '+str(failCount) +' times: retrying..'
+            else:
+                print '\t\tuploading contents of '+exportPathCombined_country+' to S3 bucket '+exportPathCombined_country.split('/')[-2].lower()+' failed '+str(failCount) +' times: GIVING UP - FILE CONTENTS MAY NOT ALL HAVE COPIED!!'
+
+print "FINISHED: ECRUNSCRIPT_combineDistribExtractions\n"
+
