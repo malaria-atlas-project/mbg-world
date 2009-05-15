60d7fd2cc0121ee87f2148a5287d6fc83e7f2273
PWG <peter.gething@zoo.ox.ac.uk>
2009-05-15 13:10:15 +0100

    minor

--- mbgw-scripts/ECDISTRIBUTE_combineDistribExtractions.py
+++ mbgw-scripts/ECDISTRIBUTE_combineDistribExtractions.py
@@ -7,13 +7,13 @@ import numpy as np
 #S=S3(keyPath='/home/pwg/mbg-world/mbgw-scripts/s3code.txt')
 
 # define ID of reservation that contains the instances we will use on EC2
-RESERVATIONID = 'r-ef5dc086'
+RESERVATIONID = 'r-3356cb5a'
 
 # set job distribution parameters
 NINSTANCES = 1
 MAXJOBSPERINSTANCE = 1
 MAXJOBTRIES = 1 #maximum number of tries before we give up on any individual job
-STDOUTPATH = '/home/pwg/mbg-world/extraction/CombinedOutputSTDERROUT/'
+STDOUTPATH = '/home/pwg/mbg-world/extraction/CombinedOutputSTDOUTERR/'
 
 # define files to upload to instance before any execution
 UPLOADFILES=['amazon_joint_sim.py','/home/pwg/mbg-world/mbgw-scripts/cloud_setup.sh','/home/pwg/mbg-world/mbgw-scripts/s3code.txt']
@@ -22,7 +22,7 @@ UPLOADFILES=['amazon_joint_sim.py','/home/pwg/mbg-world/mbgw-scripts/cloud_setup
 INITCMDS=['bash /root/cloud_setup.sh']
 
 # construct commands list
-CMDS = ['"cd mbg-world/mbgw-scripts/;python ECRUNSCRIPT_combineDistribExtractions.py False False True"'] 
+CMDS = ['"cd mbg-world/mbgw-scripts/;python ECRUNSCRIPT_combineDistribExtractions.py True True True"'] 
 
 # finally, call local function map_jobs from amazon_ec module to distribute these jobs on EC2
 returns = map_jobs(RESERVATIONID,NINSTANCES,MAXJOBSPERINSTANCE,MAXJOBTRIES,cmds=CMDS, init_cmds=INITCMDS,upload_files=UPLOADFILES, interval=20,shutdown=False,STDOUTPATH=STDOUTPATH)    
