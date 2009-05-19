fa0fa31d178d9832004ed0cb878c3066b56858aa
PWG <peter.gething@zoo.ox.ac.uk>
2009-05-19 08:31:32 +0100

    minor

--- mbgw-scripts/extract_PYlib.py
+++ mbgw-scripts/extract_PYlib.py
@@ -23,6 +23,11 @@ r.source('extract_Rlib.R')
 expandGridResPY=r['expandGridRes']
 
 # import parameters from param file
+import sys
+REGION = str(sys.argv[1])
+if REGION=="AF": from extract_params_AF import *
+if REGION=="AM": from extract_params_AM import *
+if REGION=="AS": from extract_params_AS import *
 #from extract_params import *
 
 ##############################TEMPPLACEHOLDER
