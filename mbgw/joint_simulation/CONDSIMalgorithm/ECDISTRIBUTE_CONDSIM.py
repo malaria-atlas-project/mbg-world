c28ddc7ec72f7b1a41c504416d811e51f386aa1e
PWG <peter.gething@zoo.ox.ac.uk>
2009-06-16 09:37:28 +0100

    added files for running CONDSIM on amazon

---
+++ mbgw/joint_simulation/CONDSIMalgorithm/ECDISTRIBUTE_CONDSIM.py
@@ -0,0 +1,47 @@
+# example command line:
+# run ECDISTRIBUTE_CONDSIM r-6d98ec04 CONDSIM_params_AF.py 15
+
+# import libraries
+from map_utils import amazon_ec
+from map_utils import S3
+from map_utils import checkAndBuildPaths
+import numpy as np
+import time
+import sys
+
+# deal with system arguments (expects two)
+RESERVATIONID = sys.argv[1]  ## defines ID of reservation that contains the instances we will use on EC2
+PARAMFILE_PY = sys.argv[2]  ## defines name of python file housing the parmeter definitions (e.g. extract_params_AF.py)
+PARAMFILE_R = sys.argv[3]  ## defines name of R file housing additoinal parmeter definitions for conditoinal simulation R scripts
+
+# initialise amazon S3 key object 
+S=S3(keyPath='/home/pwg/mbg-world/mbgw-scripts/s3code.txt')
+
+# set job distribution parameters
+NINSTANCES = 1
+MAXJOBSPERINSTANCE = 1
+MAXJOBTRIES = 1 #maximum number of tries before we give up on any individual job
+STDOUTPATH = '/home/pwg/mbg-world/stdout_CONDSIM/DistributedOutputSTDOUTERR_'+str(PARAMFILE_PY.partition('.')[0])+'_'+str(time.ctime())+'/'
+checkAndBuildPaths(STDOUTPATH,VERBOSE=True,BUILD=True)
+
+# set realization number parameters
+n_total = 1 #600
+iter_per_job = 1
+NJOBS = n_total / iter_per_job
+
+# define files to upload to instance (from local machine) before any execution
+UPLOADFILES=['/home/pwg/mbg-world/mbgw-scripts/cloud_setup.sh','/home/pwg/mbg-world/mbgw-scripts/s3code.txt']
+
+# define any initialisation commands to exctue on instance before main job
+INITCMDS=['bash /root/cloud_setup.sh','"cd /root/mbg-world/mbgw/joint_simulation/CONDSIMalgorithm/;python CONDSIM_defineParameterFile.py '+str(PARAMFILE_PY)+';python ECRUNSCRIPT_CONDSIM_PREDOWNLOAD.py True True True"']
+
+# construct main commands list
+CMDS = ['"cd /root/mbg-world/mbgw/joint_simulation/CONDSIMalgorithm/;python ECRUNSCRIPT_CONDSIM.py %i %i %i %i"'%(i,iter_per_job,NJOBS,PARAMFILE_R) for i in xrange(NJOBS)]
+
+# finally, call local function map_jobs from amazon_ec module to distribute these jobs on EC2
+startTime = time.time()
+returns = amazon_ec.map_jobs(RESERVATIONID,NINSTANCES,MAXJOBSPERINSTANCE,MAXJOBTRIES,cmds=CMDS, init_cmds=INITCMDS,upload_files=UPLOADFILES, interval=20,shutdown=False,STDOUTPATH=STDOUTPATH)    
+endTime = time.time()-startTime
+
+print 'total run time for '+str(NJOBS)+' jobs with iter_per_job='+str(iter_per_job)+' on '+str(NINSTANCES)+' instances, with '+str(MAXJOBSPERINSTANCE)+' jobs per instance was: '+str(endTime)
+
---
+++ mbgw/joint_simulation/CONDSIMalgorithm/examineRealization.py
@@ -0,0 +1,144 @@
+# example call
+# run examineRealization "/home/pwg/Realizations/nokrige.hdf5" 0 0 False TRUE 1
+
+# import python libraries
+from rpy import *
+import numpy as np
+import copy as cp
+import pymc as pm
+import tables as tb
+import mbgw
+from pr_incidence import *
+import time
+import st_cov_fun
+from mbgw.joint_simulation import *
+from math import sqrt
+from mbgw import correction_factors
+from map_utils import getAsciiheaderFromTemplateHDF5
+from map_utils import exportAscii
+
+from getGridCovarianceInY import * 
+# import R function
+r.source('/home/pwg/mbg-world/mbgw-scripts/extract_Rlib.R')
+plotMapPY=r['plotMap']
+flipVerticalPY=r['flipVertical']
+r.source('temptestcov.R')
+temptestcovPY=r['testRcov']
+
+# deal with system arguments
+filename = sys.argv[1]
+Rel = int(sys.argv[2]) 
+Month = int(sys.argv[3]) 
+conditioned = sys.argv[4]
+flipVertical = sys.argv[5]
+
+#print(filename)
+#print(Rel)
+
+paramfileINDEX = int(sys.argv[6])
+
+
+# set input params
+#Rel = 0 
+#filename = "/home/pwg/Realizations/realizations_mem_100000000_QRYPFPR010708_Africa_Run_9.10.2008_iterations_6_7.hdf5"
+
+
+# define basic parameters
+slices=[slice(None,None,None), slice(None,None,None), slice(Month,Month+1,None)]
+hf = tb.openFile(filename)    
+hr = hf.root
+
+slices = tuple(slices)     
+n_realizations = 1
+n_rows=len(hr.lat_axis)
+n_cols=len(hr.lon_axis)
+N_facs = int(1e5)
+N_years = (slices[2].stop - slices[2].start)/12.
+
+
+# Pull out parasite rate chunk (i.e. import n months of block)    
+slices = tuple(slices)  
+tot_slice = (slice(Rel,Rel+1,None),) + slices    
+
+n_months = tot_slice[3].stop - tot_slice[3].start
+f_chunk = np.zeros(1*n_cols*n_rows*n_months).reshape(1,n_cols,n_rows,n_months)
+subsetmonth=0 
+
+#print tot_slice
+#print f_chunk[:,:,:,subsetmonth]
+
+for mm in xrange(tot_slice[3].start,tot_slice[3].stop):
+    f_chunk[:,:,:,subsetmonth] = hr.realizations[tot_slice[0],tot_slice[1],tot_slice[2],mm]
+    subsetmonth=subsetmonth+1
+f_chunk = f_chunk[::-1,:,::-1,:].T[:,:,:,0]   
+f_chunk[f_chunk==-9999]=nan
+
+
+# plot this grid
+r.X11(width=10,height=4)
+r.par(mfrow=(1,3))
+plotMapPY(f_chunk.squeeze(),flipVertical=flipVertical)
+
+# compare global variance to parameter draw
+observedVar = round(np.var(f_chunk[np.isnan(f_chunk)==False]),3)
+theoreticalVar = ((hr.PyMCsamples.col('amp')[Rel])**2)
+varString = 'observedVar = :'+str(observedVar)+';  amp^2 =: '+str(theoreticalVar)
+print varString
+
+# plot histogram
+junk=r.hist(f_chunk[np.isnan(f_chunk)==False],main=varString,xlab="",ylab="")
+
+# calculate and plot empirical covariance function in N-S direction
+gridIN = cp.deepcopy(f_chunk).squeeze()
+
+if conditioned=="False": meanIN=0
+if conditioned=="True": meanIN = hr.PyMCsamples.col("m_const")[Rel] + (hr.PyMCsamples.col("t_coef")[Rel]*hr.t_axis[Month])
+cellWidth=5/6378.137
+covDict = getGridCovarianceInY(gridIN,meanIN,cellWidth)    
+
+# obtain theoretical covariance function from input MCMC paramater values: pymc method
+C = hr.group0.C[Rel]
+xplot = covDict['RadDist']
+yplot1 = C([[0,0,0]], np.vstack((np.zeros(len(xplot)),xplot,np.zeros(len(xplot)))).T)
+yplot1 = np.asarray(yplot1).squeeze()
+
+# obtain theoretical covariance function from input MCMC paramater values: R method
+Scale=hr.PyMCsamples.col("scale")[Rel]
+amp=hr.PyMCsamples.col("amp")[Rel]
+inc=hr.PyMCsamples.col("inc")[Rel]
+ecc=hr.PyMCsamples.col("ecc")[Rel]
+t_lim_corr=hr.PyMCsamples.col("t_lim_corr")[Rel]
+scale_t=hr.PyMCsamples.col("scale_t")[Rel]
+sin_frac=hr.PyMCsamples.col("sin_frac")[Rel]
+
+CfromR=temptestcovPY(xplot,np.zeros(len(xplot)),np.zeros(len(xplot)),Scale,amp,inc,ecc,t_lim_corr,scale_t,sin_frac,paramfileINDEX)
+yplot = CfromR[0,:]
+
+
+# plot
+
+ymax = max(np.max(covDict['E_cov']),np.max(xplot),np.max(yplot))
+ymin = min(np.min(covDict['E_cov']),np.min(xplot),np.min(yplot))
+
+r.plot(covDict['RadDist'],covDict['E_cov'],xlab="radians",ylab="C",main=str(paramfileINDEX),ylim=(ymin,ymax))    
+r.lines(xplot,yplot1,col=2)
+r.lines(xplot,yplot,col=3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
