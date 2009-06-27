a90935317635d4d2f26e2d2d0e34b5170f798028
PWG <peter.gething@zoo.ox.ac.uk>
2009-06-27 09:04:43 +0100

    minor

--- mbgw/joint_simulation/supervisor.py
+++ mbgw/joint_simulation/supervisor.py
@@ -327,8 +327,8 @@ def create_realization(outfile_root,real_index, C,C_straightfromtrace, mean_onda
         index = where_in[i]
         pdata[index] = out_arr[real_index, grid_shape[1]-1-data_mesh_indices[index,1], data_mesh_indices[index,0], data_mesh_indices[index,2]]
 
-    from IPython.Debugger import Pdb
-    Pdb(color_scheme='Linux').set_trace()
+    #from IPython.Debugger import Pdb
+    #Pdb(color_scheme='Linux').set_trace()
 
     # jointly simulate at data points conditional on block    
 
