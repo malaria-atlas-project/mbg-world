8c920acab6f768fdb7c968564c8d0c84eea4c3e0
PWG <peter.gething@zoo.ox.ac.uk>
2009-07-21 10:20:47 +0100

    minor

--- mbgw/joint_simulation/CONDSIMalgorithm/test_instance_setup.py
+++ mbgw/joint_simulation/CONDSIMalgorithm/test_instance_setup.py
@@ -1,43 +1,9 @@
 print 'Starting test script.py...'
 
-# import libraries
-import numpy as np
-import pymc as pm
-import mbgw
-import time
-from mbgw.joint_simulation import *
-import tables as tb
-import mbgw.master_grid as mg
-import os,sys
-from map_utils import S3
-from CONDSIM_params import *
-
-os.chdir('..')
-
-
-import numpy as np
-import pymc as pm
-import mbgw
-from mbgw.joint_simulation import *
-import tables as tb
-from fast_krige import *
-import st_cov_fun
-from parse_and_check import *
-import time
-import copy as cp
-from mbgw.master_grid import *
-from mbgw import auxiliary_data
-import os
-curpath = os.getcwd()
-import mbgw
-mbgw_root = __root__ = mbgw.__path__[0]
-r_path = mbgw_root+'/joint_simulation/CONDSIMalgorithm'
-os.chdir(r_path)
-from examineRealization import *
-from rpy import r
-r.source("CONDSIMpreloop.R")
-r.source("CONDSIMmonthloop.R")
-r.source('MVRNORM.R')
-mvrnormPY=r['MVRNORM']
+import pymc
+import mbg-world
+import generic-mbg
+import st-cov-fun
+import map_utils
 
 print 'DONE test script.py'
