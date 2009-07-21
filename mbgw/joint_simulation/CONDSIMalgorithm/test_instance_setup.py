8d3f90784e2bde4368967c63b4c168d106af6039
PWG <peter.gething@zoo.ox.ac.uk>
2009-07-21 10:15:11 +0100

    minor

--- mbgw/joint_simulation/CONDSIMalgorithm/test_instance_setup.py
+++ mbgw/joint_simulation/CONDSIMalgorithm/test_instance_setup.py
@@ -1,11 +1,6 @@
 print 'Starting test script.py...'
 
 # import libraries
-from map_utils import checkAndBuildPaths
-from map_utils import S3
-from CONDSIM_params import *
-import sys
-
 import numpy as np
 import pymc as pm
 import mbgw
@@ -13,16 +8,28 @@ import time
 from mbgw.joint_simulation import *
 import tables as tb
 import mbgw.master_grid as mg
-import os
+import os,sys
+from map_utils import S3
+from CONDSIM_params import *
+
+cd ..
 
 
+import numpy as np
+import pymc as pm
+import mbgw
+from mbgw.joint_simulation import *
+import tables as tb
 from fast_krige import *
 import st_cov_fun
 from parse_and_check import *
+import time
 import copy as cp
 from mbgw.master_grid import *
 from mbgw import auxiliary_data
+import os
 curpath = os.getcwd()
+import mbgw
 mbgw_root = __root__ = mbgw.__path__[0]
 r_path = mbgw_root+'/joint_simulation/CONDSIMalgorithm'
 os.chdir(r_path)
@@ -32,12 +39,5 @@ r.source("CONDSIMpreloop.R")
 r.source("CONDSIMmonthloop.R")
 r.source('MVRNORM.R')
 mvrnormPY=r['MVRNORM']
-
-os.chdir(curpath)
-import scipy
-from scipy import ndimage, mgrid
-from map_utils import grid_convert
-
-S=S3(keyPath) # initialise key object
 
 print 'DONE test script.py'
