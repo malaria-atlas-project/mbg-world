58cfc1b6bf6ee9bd7dda1ba23f2bac590c42c501
PWG <peter.gething@zoo.ox.ac.uk>
2010-01-06 09:21:00 +0000

    changes from condsim2 branch - namibia and afghan limits

--- mbgw/master_grid.py
+++ mbgw/master_grid.py
@@ -9,28 +9,98 @@ xllc = -180
 yllc = -57
 cellsize = 0.04166665
 
-AM_lims = {'topRow': 1494,
-'bottomRow': 2602,
-'leftCol': 2038,
-'rightCol': 3401}
+#<<<<<<< Updated upstream:mbgw/master_grid.py
+#AM_lims = {'topRow': 1494,
+#'bottomRow': 2602,
+#'leftCol': 2038,
+#'rightCol': 3401}
 
-AF_lims = {'topRow': 1393,
-'bottomRow': 2745,
-'leftCol': 3879,
-'rightCol': 5628}
+#AF_lims = {'topRow': 1393,
+#'bottomRow': 2745,
+#'leftCol': 3879,
+#'rightCol': 5628}
 
-AS_lims = {'topRow': 1018,
-'bottomRow': 2512,
-'leftCol': 5241,
-'rightCol': 8423}
+#AS_lims = {'topRow': 1018,
+#'bottomRow': 2512,
+#'leftCol': 5241,
+#'rightCol': 8423}
 
-# FIXME: Replace with real Kenya limits.
-KE_lims = {'topRow' : 789,
-'bottomRow' : 1021,
-'leftCol' : 2999,
-'rightCol' : 3203}
+## FIXME: Replace with real Kenya limits.
+#KE_lims = {'topRow' : 789,
+#'bottomRow' : 1021,
+#'leftCol' : 2999,
+#'rightCol' : 3203}
 
-rad_to_km = 6378.1/pi
+#rad_to_km = 6378.1/pi
+#=======
+AM_lims = {'topRow': 1525,
+'bottomRow': 2483,
+'leftCol': 2125,
+'rightCol': 3296}
+
+AMS1_lims = {'topRow': 2299,
+'bottomRow': 2325,
+'leftCol': 2784,
+'rightCol': 2801}
+
+AF_lims = {'topRow': 1412,
+'bottomRow': 2726,
+'leftCol': 3886,
+'rightCol': 5603}
+
+# Two test squares (S1 fits indside S2) in S malawi - includes a high and low focus and an urban area
+S1_lims = {'topRow': 2387,
+'bottomRow': 2412,
+'leftCol': 5147,
+'rightCol': 5164}
+
+S2_lims = {'topRow': 2373,
+'bottomRow': 2426,
+'leftCol': 5120,
+'rightCol': 5197}
+
+#AS_lims = {'topRow': 1018,
+#'bottomRow': 2512,
+#'leftCol': 5241,
+#'rightCol': 8423}
+
+
+AS1_lims = {'topRow': 1109,
+'bottomRow': 1910,
+'leftCol': 5575,
+'rightCol': 7366}
+
+AS2_lims = {'topRow': 1838,
+'bottomRow': 2522,
+'leftCol': 6592,
+'rightCol': 8419}
+
+# Kenya limits.
+KE_lims = {'topRow' : 1901,
+'bottomRow' : 2132,
+'leftCol' : 5135,
+'rightCol' : 5325}
+
+# Namibia limits.
+NBIA_lims = {'topRow' : 2421,
+'bottomRow' : 2716,
+'leftCol' : 4589,
+'rightCol' : 4926}
+
+# Afghanistan limits.
+AFGH_lims = {'topRow' : 1088,
+'bottomRow' : 1316,
+'leftCol' : 5767,
+'rightCol' : 6124}
+
+# very small test case
+VS_lims = {'topRow' : 1901,
+'bottomRow' : 1908,
+'leftCol' : 5135,
+'rightCol' : 5141}
+
+
+rad_to_km = 6378.1
 km_to_rad = 1./rad_to_km
 rad_to_deg = 180./pi
 deg_to_rad = 1./rad_to_deg
